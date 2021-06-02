94525e2d1da06bd01d91598593344bbdb2978de4
Nejuf <nejuf@nejuf.net>
2021-06-01 20:38:47 -0700

    Cleans up text2fpb.rb and makes it more readable

--- txt2fpb.rb
+++ txt2fpb.rb
@@ -1,49 +1,60 @@
 #!/usr/bin/env ruby
+#
+# txt2fpb.rb
+#
+# This script converts the translation text files(.txt) into the (.fpb) files loaded by the game.
+# Put translation files into ./script/en/
+# Separate dialogues(a.k.a. "lines") in the translation files by using "<>".  Whitespace between the dialogues will get trimmed.
+# Use newlines (\r\n) or "$n" to tell the game to split the dialogue onto a new line in the dialogue box.
+#
+# Usage:
+#   ./txt2fpb.rb
+
 
 puts "Starting txt2fpb.rb..."
 
+# TODO: Translate for every language folder in ./script/
 SCRIPT_DIR = "./script/en"
 FPB_DIR = "./fpb/en"
 KOREAN_FPB_DIR = "./fpb/ko"
 
-puts "SCRIPT_DIR #{SCRIPT_DIR}"
-puts "FPB_DIR #{FPB_DIR}"
-puts "KOREAN_FPB_DIR #{KOREAN_FPB_DIR}"
+#puts "SCRIPT_DIR #{SCRIPT_DIR}"
+#puts "FPB_DIR #{FPB_DIR}"
+#puts "KOREAN_FPB_DIR #{KOREAN_FPB_DIR}"
 
-script_files = Dir["#{SCRIPT_DIR}/0*.txt"] # e.g. ['script/en/0092.txt']
+translation_files = Dir["#{SCRIPT_DIR}/0*.txt"] # e.g. ['script/en/0092.txt']
 
-script_files.each do |script_filepath|
-  puts "Loading #{script_filepath}..."
-  script_filename = File.basename(script_filepath)
-  fpb_filename = script_filename.sub('.txt','.fpb')
-  script_file_contents = File.open(script_filepath,"r:ISO-8859-1", &:read)
+translation_files.each do |translation_filepath|
+  puts "Loading #{translation_filepath}..."
+  translation_filename = File.basename(translation_filepath)
+  fpb_filename = translation_filename.sub('.txt','.fpb')
+  translation_contents = File.open(translation_filepath,"r:ISO-8859-1", &:read)
   ko_file_contents = File.open("#{KOREAN_FPB_DIR}/#{fpb_filename}","r:ISO-8859-1", &:read)
 
-  puts "  Parsing source files..."
+#  puts "  Parsing source files..."
   first_4_bytes = ko_file_contents[0..3]
-  kor_count = first_4_bytes.unpack('V').first
-  header_length = 4 * 3 * kor_count
-  lele = ko_file_contents[4..(header_length+4)].unpack('V*')
-  i = 0
-  line_count = lele.count
+  dialogue_count = first_4_bytes.unpack('V').first
+  total_header_length = 3 * 4 * dialogue_count # 3x 32-bit header bytes, each being 4x 8-bit bytes
+  ko_line_headers = ko_file_contents[4..(total_header_length+4)].unpack('V*')
   string_tags = []
 
-  puts "  Harvesting string IDs..."
-  while i < line_count
-    string_tags.push(lele[i])
+#  puts "  Harvesting string IDs..."
+  i = 0
+  while i < ko_line_headers.count
+    string_tags.push(ko_line_headers[i]) # The string tags always end up being 0,1,2..etc, but we'll parse them from the korean FPB file just in case.
     i += 3
   end
 
-  puts "  Converting text to FPB string library..."
-  line_array = script_file_contents.strip.split('<>')
+#  puts "  Converting text to FPB string library..."
+  line_array = translation_contents.strip.split('<>')
 
-  puts "  Sanitizing..."
+#  puts "  Sanitizing..."
   line_array.map!{|line|line.strip}
   header = [line_array.count].pack("V*").force_encoding("ISO-8859-1")
   sub_off = 0
 
   if line_array.count != string_tags.count
-    raise StandardError.new("FATAL ERROR: The number of lines in the Korean and English files (#{script_filename}) does not match! (KO='#{string_tags.count}' EN='#{line_array.count}')")
+    raise StandardError.new("FATAL ERROR: The number of translation lines in the Korean (#{KOREAN_FPB_DIR}/#{fpb_filename}) and translation file (#{SCRIPT_DIR}/#{translation_filename}) does not match! (KO='#{string_tags.count}' EN='#{line_array.count}')")
   end
 
   line_array.each_with_index do |line,i|
